8716feec21cdf9705822a37ff8b52a5c9155cf2a
bamjun <khy5116@naver.com>
2024-11-23 15:16:04 +0900

    test

--- A_test.js
+++ A_test.js
@@ -8,33 +8,43 @@ function get_text_from_url() {
   return ''
 }
 
+
+// JSON 데이터 파싱 전 텍스트 클린업 함수
+function cleanJsonString(jsonString) {
+  return jsonString
+    .replace(/\\n/g, "") // 줄바꿈 제거
+    .replace(/\\t/g, "") // 탭 문자 제거
+    .replace(/\r?\n|\r/g, " ") // 추가 줄바꿈 문자 제거
+    .replace(/\\/g, ""); // 백슬래시 제거
+}
+
+
+
 function createKinnaverWordpress_20241123() {
   var kinNvaerContext = fetchGoogleTrendsItems()[0];
   // var kinNvaerContext = getContentKinNaverNoImage();
   if (0 === kinNvaerContext) {
-    Logger.log("이미지 있는 질문은 답변안하기.")
     return 0
   }
   var jsonTextObject = getPost(kinNvaerContext);
+  Logger.log(jsonTextObject)
+  // docsClearAddText({ contentText: jsonTextObject });
+
 
   var questionText = jsonTextObject[2].content.slice(0, -12);
 
-  var gptText1 = JSON.parse(jsonTextObject[3].content);
-  gptText1 = gptText1.title;
-  var gptText2 = JSON.parse(jsonTextObject[3].content);
-  gptText2 = gptText2.content;
+  var cleanContent1 = cleanJsonString(jsonTextObject[3].content);
+  var gptText1 = JSON.parse(cleanContent1).title;
+  var gptText2 = JSON.parse(cleanContent1).content;
 
+  var cleanContent2 = cleanJsonString(jsonTextObject[5].content);
+  var gptText3 = JSON.parse(cleanContent2).title;
+  var gptText4 = JSON.parse(cleanContent2).content;
 
-  var gptText3 = JSON.parse(jsonTextObject[5].content);
-  gptText3 = gptText3.title;
-  var gptText4 = JSON.parse(jsonTextObject[5].content);
-  gptText4 = gptText4.content;
+  var cleanContent3 = cleanJsonString(jsonTextObject[7].content);
+  var gptText5 = JSON.parse(cleanContent3).title;
+  var gptText6 = JSON.parse(cleanContent3).content;
 
-
-  var gptText5 = JSON.parse(jsonTextObject[7].content);
-  gptText5 = gptText5.title;
-  var gptText6 = JSON.parse(jsonTextObject[7].content);
-  gptText6 = gptText6.content;
 
   var totalText = questionText + '<h1>' + gptText1.toString() + '</h1><br><br>' + gptText2.toString() + '<br><br><h1>' + gptText3.toString() + '</h1><br><br>' + gptText4.toString() + '<br><br><h1>' + gptText5.toString() + '</h1><br><br>' + gptText6.toString();
 
